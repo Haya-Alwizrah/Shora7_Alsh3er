9ab711934decf9ac5274214eaffe8ebd85bd8e51
haya <hayaalwizrah@gmail.com>
2026-05-12 16:23:52 +0300

    Update DataManager.py

--- src/DataManager.py
+++ src/DataManager.py
@@ -6,15 +6,24 @@ from chromadb.utils import embedding_functions
 class DataManager:
 
     def __init__(self, files, db_path, collection_name, embed_model):
-        #self.folder_path = folder_path
-        self.embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(model_name=embed_model)
         self.client = chromadb.PersistentClient(path=db_path)
         self.collection_name = collection_name
-        self.collection = None
+
+        self.files = files
         self.chunks = []
         self.metadatas = []
-        self.files = files
 
+        self.embed_model=embed_model
+        self.embedding_func = None
+        self.collection = None        
+
+    def get_embedding_func(self):
+        if self.embedding_func is None:
+            print("Loading embedding model...")
+            self.embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(model_name=self.embed_model)
+        
+        return self.embedding_func
+    
     def split_poem_blocks(self, text):
         chunks = re.split(r"\n[-]{10,}\n",text)
 
@@ -22,32 +31,35 @@ class DataManager:
 
     def load_files(self):
         for file_path in self.files:
-                with open(file_path , "r", encoding="utf-8") as f:
-                    text = f.read()
+            with open(file_path , "r", encoding="utf-8") as f:
+                text = f.read()
 
-                chunks = self.split_poem_blocks(text)
+            chunks = self.split_poem_blocks(text)
 
-                for i, chunk in enumerate(chunks):
-                    self.chunks.append(chunk)
-                    self.metadatas.append({
-                        "source": file_path ,
-                        "chunk_id": i
-                    })
+            for i, chunk in enumerate(chunks):
+                self.chunks.append(chunk)
+                self.metadatas.append({
+                    "source": file_path ,
+                    "chunk_id": i
+                })
 
         print(len(self.chunks))
     
     def create_collection(self):
-
-        if self.collection_name in [c.name for c in self.client.list_collections()]:
-            self.client.delete_collection(self.collection_name)
-
-        self.collection = self.client.create_collection(
+        self.collection = self.client.get_or_create_collection(
             name = self.collection_name,
-            embedding_function = self.embedding_func
+            embedding_function = self.embedding_func()
         )
 
         print("collection created")
 
+    def db_exists(self):
+        try:
+            collections = self.client.list_collections()
+            return any(c.name == self.collection_name for c in collections)
+        except:
+            return False
+        
     def add_documents(self, batch_size=64):
 
         for i in range(0, len(self.chunks), batch_size):
@@ -62,7 +74,16 @@ class DataManager:
                 ids = batch_ids
             )
 
+        print("Documents added to vector DB")
+
     def prepare(self):
+        if self.db_exists():
+            print("DB already exists → skipping embedding step")
+            self.create_collection()
+            return
+        
+        print("Building DB from scratch...")
+
         self.load_files()
         self.create_collection()
         self.add_documents()
